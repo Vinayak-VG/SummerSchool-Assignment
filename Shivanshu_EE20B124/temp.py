fed19a591d99f689765f4ee9cc8548385a188c4a
Orewaochinchin <87032966+Orewaochinchin@users.noreply.github.com>
2021-07-07 10:31:02 +0530

    Update temp.py

--- Shivanshu_EE20B124/temp.py
+++ Shivanshu_EE20B124/temp.py
@@ -1,41 +1,9 @@
-def logic(dic):
-    prev ={}
-    tkeys = list(dic.keys())
-    while prev != dic:
-        prev = dic.copy()
-        for key in tkeys:
-            tmp = dic[key]
-            for item in tmp:
-                if item not in tkeys:
-                    dic.pop(key)
-                    tkeys.remove(key)
-                    break
-    for v in dic.values():
-        for i in v:
-            if i not in tkeys:    
-                tkeys.append(v)
-    return(tkeys)         
+import numpy as np
 
-# driver       
-z, n, m = map(int, input().split())
-states = []
-for i in range(z):
-    l = input().split()
-    states.append(l)
-dik = {}
-#output
-out = {}
-for _ in range(z):
-    out[states[_][0]] = states[_][n + 1:n + m + 1]
-    
-for _ in range(z - 1):
-    for l in range(_ + 1, z):  
-        values = []
-        if out[states[_][0]] == out[states[l][0]]:
-            for i in range(1, n + 1):
-                if states[_][i] > states[l][i]:
-                    values.append((states[l][i], states[_][i]))
-                elif states[_][i] < states[l][i]:
-                    values.append((states[_][i], states[l][i]))
-            dik[(states[_][0], states[l][0])] = values
-print(logic(dik))
+
+def cross(y, y_p):
+    return(np.sum(-1/100 * (y * np.log2(y_p) + (1 - y) * np.log2(1 - y_p))))
+
+y_p = np.random.uniform(0,1, size = 100)
+y = np.random.randint(2, size = 100)
+print(cross(y, y_p))
