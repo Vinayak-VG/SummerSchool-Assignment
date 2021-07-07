4755662630953653515f477fc3d1dcf281ae66a3
bijo11235 <86283091+bijo11235@users.noreply.github.com>
2021-07-07 15:17:28 +0530

    Add files via upload

---
+++ Bijo John_EE20B021/A1.1.py
@@ -0,0 +1,14 @@
+import numpy as np
+from math import log
+n = 100
+import math
+
+y = np.random.randint(2, size=n)
+y_hat = np.random.rand(n)
+y_hat = y_hat - y_hat//1
+
+O = 0.0
+for i in range(n):
+    O += y[i] * log(y_hat[i], 2) + (1 - y[i]) * log((1 - y_hat[i]), 2)
+O *= -1 / n
+print(O)
