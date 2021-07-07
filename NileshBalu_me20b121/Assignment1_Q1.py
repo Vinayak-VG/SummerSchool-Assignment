f45dd0419ca185d135073feddaba0c1794aa7924
NileshBalu <nileshbalu2002@gmail.com>
2021-07-07 08:25:18 +0530

    Assignment 1

--- NileshBalu_me20b121/Assignment1_Q1.py
+++ NileshBalu_me20b121/Assignment1_Q1.py
@@ -1,10 +1,15 @@
 import numpy as np
 import math
-x=np.random.rand(10,10)
-y=np.random.randint(0,2,(10,10))
-ct = 0.0
-for i in range(10):
-    for j in range(10):
-        ct += y[i,j]*(math.log2(x[i,j])) + (1-y[i,j])*(math.log2(1-x[i,j]))
-O = ct/(-100)
-print (O)
+n = input("Size : ")
+n = int(n)
+if n == 0:
+    print("invalid input")
+else:
+
+    x=np.random.rand(n)
+    y=np.random.randint(0,2,(n))
+    ct = 0.0
+    for i in range(n):
+        ct += y[i]*(math.log2(x[i])) + (1-y[i])*(math.log2(1-x[i]))
+    O = ct/(-n)
+    print (O)
