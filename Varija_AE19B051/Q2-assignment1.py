62e727435b8ba99e2faf6a505cdcd85960c0a8ba
Varija Doshi <66155856+Varija-Doshi@users.noreply.github.com>
2021-07-07 18:16:14 +0530

    changes in q2

--- Varija_AE19B051/Q2-assignment1.py
+++ Varija_AE19B051/Q2-assignment1.py
@@ -2,15 +2,23 @@
 
 # question 2 
 
+
+class MyClass:
+     
+    def show(self,target,arr):
+        n = len(arr)
+        d ={}
+        j = 1
+        for i in range (0,n):
+            for k in range (0,n):
+                if (arr[i] + arr[k])== target:
+                    d[j] = [i,k]
+                    j+=1
+        print(d)
+
+
 arr = list(map(int,input().split()))
 target = int(input())
-n = len(arr)
-d ={}
-j = 0
-for i in range (0,n-1):
-    for k in range (0,n):
-        if (arr[i] + arr[k])== target:
-            d[j] = [i,k]
-            j+=1
 
-print(d)
+c = MyClass()
+c.show(target,arr)
