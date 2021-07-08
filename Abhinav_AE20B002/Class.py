38409f08664f8c8e1a3a82179b9b6c6f4dec3431
abhinavps8 <87088570+abhinavps8@users.noreply.github.com>
2021-07-08 08:26:15 +0300

    Update Class.py

--- Abhinav_AE20B002/Class.py
+++ Abhinav_AE20B002/Class.py
@@ -1,1 +1,21 @@
+class finder
+  dict={}
+  count=1
+  
+  def checktarget(self,m,tar,arr)
+    for i in range (0,m)
+      for j in range (0,m)
+        if arr(i)+arr(j)==tar
+          dict.update({count:[i j]})
+          count++
+    
+arr=[]
+obj=finder()            
+n=int(input("Enter number of elements\n"))
+for i in range (0,n)
+  x=int(input())
+  arr.append(x)
+target=int(input("Enter target number")
+obj.checktarget(n,target,arr)
+print("Dictionary:\n",obj.dict)
 
