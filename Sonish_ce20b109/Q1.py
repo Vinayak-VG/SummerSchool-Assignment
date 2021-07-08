6f8a9dbac5875ae1295397d811fa238e2f611d62
Sonish Singhal <81894846+sonishsinghal@users.noreply.github.com>
2021-07-08 13:32:57 +0530

    Added ce20b109

--- Sonish_ce20b109/Q1.py
+++ Sonish_ce20b109/Q1.py
@@ -1,8 +1,5 @@
-import numpy as np
-
-N = 100
-
-y_hat = np.random.rand(N)
+n = 100
+y_hat = np.random.rand(n)
 y = np.random.randint(low = 0, high = 2 , size = (n))
 O = -np.sum(y * np.log2(y_hat) + (1 - y) * np.log2(1 - y_hat))/n
 print(O)
