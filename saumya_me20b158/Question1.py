32adaffed2370025e5bbcd7ab4e956972cf1acd8
saumya-77 <85883158+saumya-77@users.noreply.github.com>
2021-07-08 12:40:57 +0530

    Update Question1.py

--- saumya_me20b158/Question1.py
+++ saumya_me20b158/Question1.py
@@ -3,8 +3,8 @@ import math
 
 n=100
 
-y=np.random.rand(1,size=n)
-ycap=np.randon.randit(2,size=n)
+ycap=np.random.rand(1,size=n)
+y=np.randon.randit(2,size=n)
 
 array1 = np.multiply(y,np.log2(ycap))+np.multiply(1-y,np.log2(1-ycap))
 print(np.sum(array1)/(-n))
