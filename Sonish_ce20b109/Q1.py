701e612eb1a974ecd5b668e3880007d4ceac54f6
Sonish Singhal <81894846+sonishsinghal@users.noreply.github.com>
2021-07-08 13:30:26 +0530

    Update Q1.py

--- Sonish_ce20b109/Q1.py
+++ Sonish_ce20b109/Q1.py
@@ -1,12 +1,8 @@
-#!/usr/bin/python3
-
 import numpy as np
 
 N = 100
 
 y_hat = np.random.rand(N)
-y = np.random.randint(0, 2, N)
-
-cross_entropy = (-1/N)*np.sum(y*np.log2(y_hat) + (1 - y)*(1 - np.log2(1 - y_hat)))
-
-print(f"{cross_entropy=}")
+y = np.random.randint(low = 0, high = 2 , size = (n))
+O = -np.sum(y * np.log2(y_hat) + (1 - y) * np.log2(1 - y_hat))/n
+print(O)
