1eaeedcfdee8c3a3d4d3cf31b5bc9d6f32d3797a
Bindusara <43317025+bindusara-reddy@users.noreply.github.com>
2021-07-08 08:42:16 +0530

    solution1.py

--- Bindusara_na20b013/solution1.py
+++ Bindusara_na20b013/solution1.py
@@ -3,9 +3,6 @@ import numpy as np
 n=5
 y=np.random.randint(0,2,n)
 y_hat=np.random.rand(n)
-n = 7
-y_hat = np.array([0.45, 0.10, 0.99, 0.32, 0.78, 0.81,0.27])
-y = np.array([1, 0, 1, 1, 1, 0, 0])
 ones=np.ones(n)
 x=np.array(n)
 x=(y*(np.log2(y_hat)))+((ones-y)*(np.log2(ones-y_hat)))
