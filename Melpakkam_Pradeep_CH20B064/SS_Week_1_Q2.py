356078989d9cba4390aa95c0c7ffbce105025e6f
MelpakkamPradeep <86355987+MelpakkamPradeep@users.noreply.github.com>
2021-07-09 10:05:10 +0530

    Update SS_Week_1_Q2.py

--- Melpakkam_Pradeep_CH20B064/SS_Week_1_Q2.py
+++ Melpakkam_Pradeep_CH20B064/SS_Week_1_Q2.py
@@ -1,17 +1,34 @@
-def check(a):
-    counter = 0
-    dict = {}
-    for i in range(len(a)):
-        for j in range(len(a)):
-            if a[i] + a[j] == target:
-                dict.update({counter: (i, j)})
-                counter += 1
-    print(dict)
+class Pair:
+    def __init__(self, a, target):
+        self.a = a
+        self.target = target
+    def check(a):
+        counter = 0
+        dict = {}
+        for i in range(len(pair.a)):
+            for j in range(len(pair.a)):
+                if pair.a[i] + pair.a[j] == target:
+                    dict.update({counter: (i, j)})
+                    counter += 1
+        print(dict)
 
 
 target = int(input("Target: "))
 a = list(map(int, input().split()))
-check(a)
+pair = Pair(a, target)
+pair.check()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
